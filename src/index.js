ec86a10e3b56dcd3d323627adb4ed4fbacbef11a
matrexdev <matrexwutsu@gmail.com>
2023-06-18 15:29:53 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
-import "./components/buttons";
+import "./components/Buttons";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
