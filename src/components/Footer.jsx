34db350f1eaf280f7eba86439be03bf2cc9ce1bd
matrexdev <matrexwutsu@gmail.com>
2024-03-28 21:03:15 +0300

    asd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function footer() {
   return (
     <footer className="footer">
       <p className="footerText">
-        This website is open source @{" "}
+        This website is open source at{" "}
         <a
           href="https://github.com/matrexdev/matrex.dev"
           rel="noreferrer noopener"
