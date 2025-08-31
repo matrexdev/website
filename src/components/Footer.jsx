b27402ae1c2368129b7be6e810b642eea6910789
matrexdev <matrexwutsu@gmail.com>
2025-08-31 21:02:16 +0300

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,7 @@ export default function footer() {
     <footer className="footer">
       <p className="footerText">
         This website is open source at{" "}
-        <a
-          href="https://github.com/matrexdev/matrex.dev"
-          rel="noreferrer noopener"
-          target="_blank"
-        >
+        <a href="https://github.com/matrexdev/website" rel="noreferrer noopener" target="_blank">
           Github
         </a>
       </p>
